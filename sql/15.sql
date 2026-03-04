34c0f454bf1def50d168daf54cb249e72b693f09
Mateen Mokhtarani <mateenmok@gmail.com>
2026-03-03 20:23:35 -0800

    solve all sql problems

--- sql/15.sql
+++ sql/15.sql
@@ -1,4 +1,11 @@
 /*
  * Use a JOIN to count the number of English language films in each category.
- * Use table category, film_category, film, and language.
  */
+SELECT c.name, count(*) AS sum
+FROM film f
+JOIN film_category fc ON f.film_id = fc.film_id
+JOIN category c ON fc.category_id = c.category_id
+JOIN language l ON f.language_id = l.language_id
+WHERE l.name = 'English'
+GROUP BY c.name
+ORDER BY c.name;
--- sql/16.sql
+++ sql/16.sql
@@ -1,6 +1,10 @@
 /*
- * Use a JOIN to order the films by most profitable,
- * where the profit is the total amount that customers have paid for the film.
- * Use tables payment, rental, inventory, and film. 
+ * Use a JOIN to order the films by most profitable.
  */
-
+SELECT f.title, sum(p.amount) AS profit
+FROM film f
+JOIN inventory i ON f.film_id = i.film_id
+JOIN rental r ON i.inventory_id = r.inventory_id
+JOIN payment p ON r.rental_id = p.rental_id
+GROUP BY f.title
+ORDER BY profit DESC;
